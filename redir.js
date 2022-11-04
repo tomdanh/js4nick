2612625c44473441a87ea76f1ac1b7e25e607406
tomdanh <71402181+tomdanh@users.noreply.github.com>
2022-11-04 08:53:26 +0000

    Create redir.js

---
+++ redir.js
@@ -0,0 +1,21 @@
+// Would you be able to suggest a best way to edit this JS, where PLACEID variable would be dynamically pulled from the url from a campaign_id=12345 url string? //
+
+var offer_url = "https://google.com/click.php?lp=1";
+var split_url = "https://google.com/click.php?key=oxv0nt5450qw39c03wro&redir=PLACEID";
+function go() {
+    window.onbeforeunload = null;
+    window.open(offer_url);
+    document.location.assign(split_url);
+}
+var bbURL = "https://google.com/click.php?key=oxv0nt5450qw39c03wro&redir=PLACEID";
+!(function () {
+    var a;
+    try {
+        for (a = 0; 3 > a; ++a) {
+            history.pushState({}, "", "#");
+        }
+        onpopstate = function (c) {
+            c.state && location.replace(bbURL);
+        };
+    } catch (b) {}
+})();
